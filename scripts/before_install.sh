ddfdb796a0a60d68926f769ccdcf531196cb3dbc
Santhosh K <santhoshkaruppusamy55@gmail.com>
2025-08-07 17:55:37 +0530

    scripts updation

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,4 +7,4 @@ if ! command -v docker &> /dev/null; then
     sudo service docker start
     sudo usermod -aG docker ec2-user
 fi
-docker login -u "$(aws ssm get-parameter --name /docker/username --region ap-south-1 --query Parameter.Value --output text)" --password "$(aws ssm get-parameter --name /docker/password --region ap-south-1 --query Parameter.Value --output text)"
+echo "$(aws ssm get-parameter --name /docker/password --region ap-south-1 --query Parameter.Value --output text --with-decryption)" | docker login -u "$(aws ssm get-parameter --name /docker/username --region ap-south-1 --query Parameter.Value --output text)" --password-stdin
