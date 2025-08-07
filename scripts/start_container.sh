5c5837ff8b8288ede72d4ceb3e79bcf302eb19b5
Santhosh K <santhoshkaruppusamy55@gmail.com>
2025-08-07 17:05:28 +0530

    deploy updation

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 set -e
-docker pull santhoshkaruppusamy/website:tagname
-docker run -d -p 80:80 santhoshkaruppusamy/website
+echo "Starting new container"
+# Retrieve repo URL from Parameter Store
+REPO_URL=$(aws ssm get-parameter --name /docker/repo/url --region ap-south-1 --query Parameter.Value --output text)
+docker pull "${REPO_URL}:latest"
+docker run -d --name my-app-container -p 80:80 "${REPO_URL}:latest"
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 set -e
-container_id=$(docker ps -q)
-docker rm -f $container_id
+echo "Stopping existing container"
+container_id=$(docker ps -q -f name=my-app-container)
+if [ -n "$container_id" ]; then
+    docker stop $container_id
+    docker rm -f $container_id
+fi
