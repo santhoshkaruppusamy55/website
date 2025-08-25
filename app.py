fba7feae1c0ed5063e594b42aa593b5f4d3000ad
Santhosh K <santhoshkaruppusamy55@gmail.com>
2025-08-25 11:54:06 +0530

    New page updation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-app = Flask(__name__, static_folder='static')  # Specifies the static folder
+
+app = Flask(__name__)  # Removed static_folder since it's not needed yet
 @app.route('/')
 def home():
     return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
